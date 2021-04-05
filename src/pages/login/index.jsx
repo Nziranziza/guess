426bce8d82f95834c22e4c9005c04555d6f90ee4
Nziranziza <nziranzizadaniel@gmail.com>
2021-04-05 22:06:46 +0200

    apply page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,52 +1,49 @@
 import React, { useState } from "react";
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
 import "./index.scss";
 
 const Login = () => {
   const [credentials, setCredentials] = useState({
-    username: '',
-    password: ''
+    username: "",
+    password: "",
   });
   const history = useHistory();
 
   const onInputChange = (key, value) => {
     setCredentials({
       ...credentials,
-      [key]: value
-    })
+      [key]: value,
+    });
   };
 
   const isDisabled = () => {
-    return !credentials.password || !credentials.username
-  }
+    return !credentials.password || !credentials.username;
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    localStorage.setItem('credentials', JSON.stringify(credentials));
-    history.push('apply')
+    localStorage.setItem("credentials", JSON.stringify(credentials));
+    history.push("apply");
   };
 
   return (
     <div className="login-container">
       <h1>Login to Guess Game</h1>
       <form className="login-form">
-        <input 
-        type="text" 
-        placeholder="Username"
-        value={credentials.username}
-        onChange={(e) => onInputChange('username', e.target.value)}
+        <input
+          type="text"
+          placeholder="Username"
+          value={credentials.username}
+          onChange={(e) => onInputChange("username", e.target.value)}
         />
-        <input 
-        type="password" 
-        placeholder="Password"
-        value={credentials.password}
-        onChange={(e) => onInputChange('password', e.target.value)}
+        <input
+          type="password"
+          placeholder="Password"
+          value={credentials.password}
+          onChange={(e) => onInputChange("password", e.target.value)}
         />
-        <button
-         disabled={isDisabled()}
-         onClick={onSubmit}
-        >
+        <button disabled={isDisabled()} onClick={onSubmit}>
           Login
         </button>
       </form>
